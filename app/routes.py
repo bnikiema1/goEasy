716342a268af342443d575d17a839eb538af409b
Abdullah <nnbcode13@gmail.com>
2025-08-15 20:10:49 -0500

    more templates work

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from . import app
 from flask import render_template, redirect, flash, request
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    
+    if request.method == 'POST':
+        print(request.form)
     return render_template('login.html', title='Login')
 
 @app.route('/signup')
--- config.py
+++ config.py
@@ -5,3 +5,5 @@ base_dir = Path(__file__).parent
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'project.db')
+    
