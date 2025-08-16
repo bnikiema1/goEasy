4a08148e6c77e4032d0b1f6a6079fd19321e67d0
Abdullah <nnbcode13@gmail.com>
2025-08-15 19:38:28 -0500

    updated config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ base_dir = Path(__file__).parent
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret key'
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(base_dir, 'project.db')
+    
+    WTF_CSRF_ENABLED = True
+    WTF_CSRF_SECRET_KEY = os.environ.get('CSRF_SECRET_KEY') or 'csrf key'
