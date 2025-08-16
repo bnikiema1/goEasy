6a2546465224cbd1cb878e9e5d3624743d244d6c
Abdullah <nnbcode13@gmail.com>
2025-08-15 19:32:03 -0500

    added csrf protection

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from config import Config
-from .extensions import db, migrate
+from .extensions import db, migrate, csrf
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 migrate.init_app(app,db)
+csrf.init_app(app)
 
 from .routes import *
 from .models import User
